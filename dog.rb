011ad9cb472d494905049880b84d148d47b24796
Katia <katia.shukh@gmail.com>
2017-12-09 01:55:38 +0000

    first commit

---
+++ dog.rb
@@ -0,0 +1,25 @@
+puts 'I am in the dog file'
+require_relative 'mammal'
+
+class Dog < Mammal
+
+    def pet
+        @@health+=5
+        self
+    end
+
+    def walk
+        @@health-=1
+        self
+    end
+
+    def run
+        @@health-=10
+        self
+    end
+
+end
+
+puppy = Dog.new.walk.walk.walk
+puppy.run.run.pet
+puppy.display_health
